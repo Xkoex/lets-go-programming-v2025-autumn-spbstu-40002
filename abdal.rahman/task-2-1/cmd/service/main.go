26731a4ec2c36b2b6588313b5ea9b853d0b9691b
abdal.rahman <abdabublal1@gmail.com>
2026-01-04 14:12:26 +0200

    [TASK-2-1] Fix all linting errors

--- abdal.rahman/task-2-1/cmd/service/main.go
+++ abdal.rahman/task-2-1/cmd/service/main.go
@@ -54,30 +54,34 @@ func main() {
 
 	if _, err := fmt.Scan(&numDeps); err != nil || numDeps < MinDepartments || numDeps > MaxDepartments {
 		fmt.Println("Error:", ErrReadDepartments)
+
 		return
 	}
 
-	for depIndex := 0; depIndex < numDeps; depIndex++ {
+	for range numDeps {
 		var numEmps int
 
 		if _, err := fmt.Scan(&numEmps); err != nil {
 			fmt.Println("Error:", ErrReadEmployees)
+
 			return
 		}
 
 		dept := DeptTemp{Min: TempMin, Max: TempMax}
 
-		for empIndex := 0; empIndex < numEmps; empIndex++ {
+		for range numEmps {
 			var operation string
 			var temperature int
 
 			if _, err := fmt.Scan(&operation, &temperature); err != nil {
 				fmt.Println("Error: invalid input")
+
 				return
 			}
 
 			if err := dept.UpdateTemp(operation, temperature); err != nil {
 				fmt.Println("Error:", err)
+
 				return
 			}
 
